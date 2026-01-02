baefee1b490ae21b869159860fa488336c44453c
Leonardo lima aranha <squallyspyder@gmail.com>
2026-01-02 00:25:25 +0000

    chore: add dist/ to gitignore and remove from repo
    - dist/ should be generated during build, not committed

--- dist/src/lib/sandbox-logs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sandboxLogEmitter = void 0;
-exports.emitSandboxLog = emitSandboxLog;
-const events_1 = require("events");
-exports.sandboxLogEmitter = new events_1.EventEmitter();
-function emitSandboxLog(event) {
-    exports.sandboxLogEmitter.emit('log', event);
-}
